520d0a6201c0c251539b44101ca9b32ed079eb07
Reoup <71483084+Reoup@users.noreply.github.com>
2021-07-29 13:40:26 +0900

    commit

---
+++ src/componenets/search_bar.js
@@ -0,0 +1,27 @@
+import React, {Component} from 'react';
+
+class SearchBar extends Component {
+    constructor(props){ // 클래스가 생성될 때마다 자동적으로 실행
+        super(props); // 호출하면서 이 부모 클래스의 메소드를 호출할 수 있음
+
+        this.state = {term: ''}; // term은 search term을 의미하는 프로퍼티
+    }
+
+    render(){
+        return (
+            <div className="search-bar">
+            <input 
+            value = {this.state.term}
+            onChange={event => this.onInputChange(event.target.value)}  // onInputChange와 새 인풋값을 호출함
+            />
+            </div>
+        );
+    }
+
+    onInputChange(term){
+        this.setState({term}); // 1. setState
+        this.props.onSearchTermChange(term); // 2. onSearchTermChange 콜백 함수를 호출하는 것
+    }
+}
+
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,55 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTsearch from 'youtube-api-search';
+import SearchBar from './componenets/search_bar';
+import VideoList from './componenets/video_list';
+import VideoDetail from './componenets/video_detail';
+
+const API_KEY = 'AIzaSyCpNmknKlReIaS2v8mVfsY6SS9gHS81nIQ';
+
+
+
 // Create a new component. This componenet should produce
 // some HTML
 
-const App = function(){
-  return <div>Hi!</div>;
+class App extends Component {
+constructor(props){
+  super(props);
+
+  this.state = { 
+    videos: [],
+    selectedVideo: null
+  };
+
+  this.videoSearch('surfboards'); // 
+
 }
+
+videoSearch(term){ // 유튜브 검색을 리펙토링하여 하나의 메소드로 만듦
+  YTsearch({key: API_KEY, term: term}, (videos) => { // 하나의 검색어를 가짐
+    this.setState({
+      videos: videos,
+      selectedVideo: videos[0]
+    }); // this.setState({videos: videos});
+  });
+}
+
+  render() {
+    return (
+      <div>
+        <SearchBar onSearchTermChange={term => this.videoSearch(term)}/> 
+        <VideoDetail video={this.state.selectedVideo}/>
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+          videos={this.state.videos}/>
+      </div>
+    );
+  }
+}
+// <SearchBar onSearchTermChange={term => this.videoSearch(term)}/> 검색 바에서 새로운 검색어로 바뀌면 이 검색 함수를 호출하게 되며 새로운 비디오를 가져옴
+
 
 // Take this componenet's generated HTML and put it
 // on the page (in the DOM)
-ReactDOM.render(<App/>);
+ReactDOM.render(<App/>, document.querySelector('.container'));
 
